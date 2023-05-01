b25f8590354bc178e1cb0f7c766a9403dbf34074
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-01 15:37:27 +0100

    My pipex May 1, 2023

---
+++ srcs/pipex_children.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_children.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 15:20:12 by diogpere          #+#    #+#             */
+/*   Updated: 2023/04/30 14:15:47 by diogpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/pipex.h"
+
+void	child_process(pipex_info *info, char **argv, char **envp)
+{
+    info->pid = fork();
+    if (info->pid < 0)
+		err_msg_exit(ERR_FORK);
+    else if (info->pid == 0)
+	{
+		handle_pipes(info, argv);
+		if (info->arg_path == 0)
+			cmd_error_exit(info->args[0]);
+		execve(info->arg_path, info->args, envp);
+		// protect here
+		// exit(0);
+	}
+}
+
+void	handle_pipes(pipex_info *info, char **argv)
+{
+	if (info->i == 2)
+	{
+		info->infile = open(argv[1], O_RDONLY);
+		if (info->infile < 0)
+			err_msg_exit(ERR_INFILE);
+		close(info->pipe[0]);
+		dup2(info->infile, STDIN_FILENO);
+		dup2(info->pipe[1], STDOUT_FILENO);
+		// maybe protect dup2s
+		close(info->infile);
+	}
+	if (info->i == 3)
+	{
+		info->outfile = open(argv[4], O_TRUNC | O_CREAT | O_RDWR, 0644);
+		if (info->outfile < 0)
+			err_msg_exit(ERR_OUTFILE);
+		close(info->pipe[1]);
+		dup2(info->outfile, STDOUT_FILENO);
+		dup2(info->pipe[0], STDIN_FILENO);
+		close(info->outfile);
+	}
+	shut_pipe(info);
+}
---
+++ srcs/pipex_main.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 15:03:37 by diogpere          #+#    #+#             */
+/*   Updated: 2023/04/28 15:24:40 by diogpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/pipex.h"
+
+void	shut_pipe(pipex_info *info)
+{
+	close(info->pipe[0]);
+	close(info->pipe[1]);
+}
+
+void	free_split(char **strings)
+{
+	int	i;
+
+	i = 0;
+	while (strings[i])
+		free(strings[i++]);
+	free(strings);
+}
+
+int	get_paths(pipex_info *data, char **envp)
+{
+	int		j;
+
+	j = -1;
+	while (envp[++j])
+	{
+		if (ft_strncmp(envp[j], "PATH=", 5) == 0)
+		{
+			data->envp_paths = ft_split(&envp[j][5], ':');
+			return (1);
+		}
+	}
+	return (-1);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+    pipex_info  *info;
+
+    if (argc != 5)
+        return (custm_err_msg(ERR_INPUT));
+    info = ft_calloc(1, sizeof(pipex_info));
+    info->i = 1;
+    if (!get_paths(info, envp))
+		return (custm_err_msg(ERR_PATHS));
+    if (pipe(info->pipe) < 0)
+		err_msg_exit(ERR_PIPE);
+    while ((++(info->i) <= argc - 2))
+    {
+        args_prep(info, argv);
+        child_process(info, argv, envp);
+        free_split(info->args);
+        free(info->arg_path);
+           
+    }
+    shut_pipe(info);
+    waitpid(-1, NULL, 0);
+    free_split(info->envp_paths);
+    free(info);
+    return 0;
+}
