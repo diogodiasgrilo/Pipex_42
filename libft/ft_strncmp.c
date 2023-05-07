4c96fabb6c882784d6f3682b72e23c3ff9f4cba4
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-07 14:10:42 +0100

    Update ft_strncmp.c May 7, 14:10

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, int n)
 	int				i;
 
 	i = 0;
-	if (n == 0 || (!*s1 && !*s2))
+	if (!s1 || !s2 || n == 0 || (!*s1 && !*s2))
 		return (0);
 	if (n < 0)
 		return (-1);
